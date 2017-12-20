f5e5ea5e17046f119fed8c9ce1201a83be001e56
smac89 <smac89@users.noreply.github.com>
2017-12-19 20:46:59 -0600

    Check for existing paths before adding to user's path

--- linuxbrew.plugin.zsh
+++ linuxbrew.plugin.zsh
@@ -1,25 +1,35 @@
+local function export_path() {
+  for p in ${(s.:.)$2}; do
+    if [[ ! "${(P)1}" =~ "$p" ]]; then
+      export "$1"="$p:${(P)1}"
+    fi
+  done
+}
+
 local function export_env() {
   local brew_prefix="$1"
-  export PATH="$brew_prefix/sbin:$brew_prefix/bin:$PATH"
-  export MANPATH="$brew_prefix/share/man:$MANPATH"
-  export INFOPATH="$brew_prefix/share/info:$INFOPATH"
-  export XDG_DATA_DIRS="$brew_prefix/share:$XDG_DATA_DIRS"
-  fpath=( "$brew_prefix/completions/zsh" $fpath )
+  if [ -d "$brew_prefix" ]; then
+    export_path 'PATH' "$brew_prefix/sbin:$brew_prefix/bin"
+    export_path 'MANPATH' "$brew_prefix/share/man"
+    export_path 'INFOPATH' "$brew_prefix/share/info"
+    export_path 'XDG_DATA_DIRS' "$brew_prefix/share"
+    fpath=( "$brew_prefix/completions/zsh" $fpath )
+  fi
 }
 
 local HOME_PREFIX="$HOME/.linuxbrew"
-local RECOMMENDED_PREFIX="/home/linuxbrew/.linuxbrew"
-
+local RECOMMENDED_PREFIX='/home/linuxbrew/.linuxbrew'
 
 export_env $HOME_PREFIX
 export_env $RECOMMENDED_PREFIX
 
 if (( ! $+commands[brew] )); then
-  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install)"
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install)"
   if [[ ! $? -eq 0 ]]; then
-      printf 'recommended install failed. try just cloning git repo? '
+      echo -n 'Recommended install failed. Try just cloning git repo? '
       if read -q; then
-          echo; git clone https://github.com/Linuxbrew/brew "$HOME_PREFIX" </dev/null >/dev/null 2>/dev/null &!
+          echo '...'
+          git clone https://github.com/Linuxbrew/brew "$HOME_PREFIX" &> /dev/null &!
       fi
   fi
 fi
